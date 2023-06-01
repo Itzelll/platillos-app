00f807db1eadf27f847e216d96dd9d2ae6f8e0db
itzelll <mmitzel2435@gmail.com>
2023-05-31 20:51:53 -0600

    reactions connection

--- src/components/reactions/LoveButton.component.js
+++ src/components/reactions/LoveButton.component.js
@@ -1,22 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import love from '../img/love.png';
 import KafkaService from "../../services/kafka.service";
 import { useAuth } from '../../context/AuthContext';
 
+const MongoDBService = require('../../services/MongoDb.service');
 
-function LoveButton({pubId}) {
-    const {user} = useAuth();
+function LoveButton({ pubId }) {
+    const { user } = useAuth();
     const [loves, setLikes] = useState(0);
     const [loved, setLiked] = useState(false);
 
-    function saveLike(e) { 
-        const uId= user.uid;    
-        const oId= pubId;
-        const rId= "love"
+
+    useEffect(() => {
+        // Crea una instancia de MongoDBService con la URL base del backend
+        const mongoDBService = new MongoDBService('http://localhost:3001');
+
+        // Define los parámetros deseados para la llamada a getReactionsByObjectAndReaction
+        const objectId = pubId;
+        const reactionId = 'love';
+
+        // Define una función asincrónica para cargar los datos
+        const fetchData = async () => {
+            try {
+                const response = await mongoDBService.getReactionsByObjectAndReaction(objectId, reactionId);
+                const data = response[0];
+                setLikes(data.n);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        // Llama a fetchData al montar o actualizar el componente
+        fetchData();
+    })
+    
+
+    function saveLike(e) {
+        const uId = user.uid;
+        const oId = pubId;
+        const rId = "love"
         KafkaService.reaction(uId, oId, rId);
         e.preventDefault();
     }
-    
+
     return (
         <div className="like-button-container">
             <button id="like"
@@ -28,8 +54,8 @@ function LoveButton({pubId}) {
                     saveLike(e, 1);
                 }}
             >
-                <img src={love} className='img' width={35} height={35} alt="" /> {loves} <br/>
-                 
+                <img src={love} className='img' width={35} height={35} alt="" /> {loves} <br />
+
             </button>
         </div>
     );
